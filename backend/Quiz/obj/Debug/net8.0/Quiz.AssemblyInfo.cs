b258d4e5dba42298038fea9fc5e880efb97e3d93
nanonano <mianistree2022@gmail.com>
2024-12-17 18:42:57 +0700

    update

--- backend/Quiz/obj/Debug/net8.0/Quiz.AssemblyInfo.cs
+++ backend/Quiz/obj/Debug/net8.0/Quiz.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Quiz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60e55553960d3184fb95121ddf56a711abbe5a89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb79c1627f3ce9c455b2d3a256ded79174d1e0bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Quiz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Quiz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
